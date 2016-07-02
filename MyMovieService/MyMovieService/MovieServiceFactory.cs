cf913a68afba09dbc1e64a779aeb19a824f6554e
yinonmanor <manoryinon@gmail.com>
2016-07-02 21:22:30 +0300

    Add files via upload

--- MyMovieService/MyMovieService/MovieServiceFactory.cs
+++ MyMovieService/MyMovieService/MovieServiceFactory.cs
@@ -8,17 +8,10 @@ namespace MyMovieService
 {
     public class MovieServiceFactory
     {
-        /// <summary> 
-        /// use http://www.omdbapi.com REStful Web Service. 
-        /// </summary> 
+        
         public static string OMDB_API = "OMDB_API";
  
-         /// <summary> 
-         /// This method get from the user the web service that he want 
-         /// to create an object, and create a singleton object. 
-         /// </summary> 
-         /// <param name="str">The desired service.</param> 
-         /// <returns>Return an object from the type of the chosen REStful Web Service.</returns> 
+         
          public static Movie getMovieDataService(string str)
          {
             Movie movie = null; 
--- MyMovieService/UnitTestMyMovieService/UnitTest1.cs
+++ MyMovieService/UnitTestMyMovieService/UnitTest1.cs
@@ -12,46 +12,102 @@ namespace MyMovieService.Tests
         ImoviesDataService service = (ImoviesDataService)MovieServiceFactory.getMovieDataService(MovieServiceFactory.OMDB_API);
         Movie movie;
 
-        /// <summary>
-        /// This method check if the web service is support the Const value.
-        /// </summary>
+        
         [TestMethod]
         public void TestConstFactory()
         {
             Assert.IsNotNull(service);
         }
 
-        /// <summary>
-        /// Check that that the return name of the movie return is what wi send
-        /// </summary>
+        
         [TestMethod]
         public void TestGetMovieName()
         {
             string testString = "The Boy";
-            movie = service.GetMovieData("The Boy");
+            movie = service.GetMovieData(testString);
             Assert.AreEqual(testString, movie.Title);
         }
-
-        /// <summary>
-        /// Disconnect the internet in order to throw an excetion ( WebException )
-        /// and check if there is a fail.
-        /// </summary>
+        
         [TestMethod]
-        public void TestXMLInternet()
+        public void TestGetMovieYear()
         {
-            ProcessStartInfo info = new ProcessStartInfo();
-            info.FileName = "ipconfig";
-            info.Arguments = "/release"; // or /release if you want to disconnect
-            info.WindowStyle = ProcessWindowStyle.Hidden;
-            Process p = Process.Start(info);
-            p.WaitForExit();
+            string testString = "Toy Story";
+            int year = 1995;
+            movie= service.GetMovieData(testString);
+            Assert.AreEqual(year, movie.Year);
+        }
 
-            Movie movie = service.GetMovieData("The Boy");
+        [TestMethod]
+        public void TestGetMovieLanguage()
+        {
+            string testString = "Toy Story";
+            string language = "English";
+            movie = service.GetMovieData(testString);
+            Assert.AreEqual(language, movie.Language);
+        }
 
-            info.Arguments = "/renew";
-            p = Process.Start(info);
+        [TestMethod]
+        public void TestGetMovieRuntime()
+        {
+            string testString = "Toy Story";
+            string runtime = "81 min";
+            movie = service.GetMovieData(testString);
+            Assert.AreEqual(runtime, movie.Runtime);
+        }
 
-            Assert.AreNotEqual(null, movie.Title);
+        [TestMethod]
+        public void TestGetMovieCountry()
+        {
+            string testString = "Toy Story";
+            string country = "USA";
+            movie = service.GetMovieData(testString);
+            Assert.AreEqual(country, movie.Country);
+        }
+
+
+        [TestMethod]
+        public void TestGetMovieType()
+        {
+            string testString = "Toy Story";
+            string type = "movie";
+            movie = service.GetMovieData(testString);
+            Assert.AreEqual(type, movie.Type);
+        }
+
+        [TestMethod]
+        public void TestGetMovieRated()
+        {
+            string testString = "forrest gump";
+            string rated = "PG-13";
+            movie = service.GetMovieData(testString);
+            Assert.AreEqual(rated, movie.Rated);
+        }
+
+        [TestMethod]
+        public void TestGetMovieActors()
+        {
+            string testString = "forrest gump";
+            string actors = "Tom Hanks, Rebecca Williams, Sally Field, Michael Conner Humphreys";
+            movie = service.GetMovieData(testString);
+            Assert.AreEqual(actors, movie.Actors);
+        }
+
+        [TestMethod]
+        public void TestGetMovieIMDBRating()
+        {
+            string testString = "forrest gump";
+            double imdbRate = 8.8;
+            movie = service.GetMovieData(testString);
+            Assert.AreEqual(imdbRate, movie.ImdbRating);
+        }
+
+        [TestMethod]
+        public void TestGetMovieIMDBId()
+        {
+            string testString = "forrest gump";
+            string  imdbId= "tt0109830";
+            movie = service.GetMovieData(testString);
+            Assert.AreEqual(imdbId, movie.ImdbID);
         }
     }
 }
